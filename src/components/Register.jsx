9ced5d44264276db3212e0859074effee2580892
lizzyjoo <jejviolinist@gmail.com>
2025-12-26 16:48:44 -0500

    styles: import styles for login and register components

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import "../styles/register.css";
 
 //need to check if users are already looged in, if so redirect to home page
 // handle for if user email already exists
